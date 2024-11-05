5b76f73781f103e33580c5b9fd221d0a7b9a4e10
Ujjal Mahata <ujjadsingh66@gmail.com>
2024-11-05 15:55:57 +0545

    ..

---
+++ Untitled-1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string.h>
+usning namespace std;
+class teacher{
+    //properties
+    string name;
+    string dept;
+    string subj;
+   double sallary;
+ //method
+ void changedept(string newdept)
+ {
+    dept=newdept;
+ }
+};
+int main()
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,18 @@
+#include<iostream.h>
+using namespace srd;
+privite:()ujjal
+{
+    cout<<my name is ujjal singh mahATA;
+
+}
+bublic:()mehata
+{
+cout<<my name is rajesh;
+cin>>sum23
+sum=obj 1+obj 2
+}
+int main()
+obj 1 ujjal;
+obj 2 mehata;
+obj 3 sum;
+return 0;
