cb202709054630bfbf237c06216f7601a7643507
Ujjal Mahata <ujjadsingh66@gmail.com>
2024-11-17 09:13:10 +0545

    simple class

---
+++ simpleclass.cpp
@@ -0,0 +1,19 @@
+
+
+#include<iostream>
+using namespace std;
+class print
+{
+ public:
+ void show()
+ {
+    cout<<"er ujjal singh mahta";
+ }
+};
+    int main()
+
+{
+ print obj;
+obj.show();
+return 0;
+}
