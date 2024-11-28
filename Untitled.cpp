d97ce1f7eb20674ac46aa7ed6fcfa3620809fe76
Ujjal Mahata <ujjadsingh66@gmail.com>
2024-11-28 11:08:55 +0545

    untilled

---
+++ Untitled.cpp
@@ -0,0 +1,24 @@
+#include<conio.h>
+using namespace std;
+
+class s;
+ privite:
+ 
+  void show()
+int a;
+cin>>a>>;
+cout<<a<<;
+
+public:
+ int b;
+ void int()
+cin>>b>>;
+cout<<b<<;
+
+ int main()
+ {
+ s;
+ s.void show();
+ s.void int();
+ return 0;
+ }
