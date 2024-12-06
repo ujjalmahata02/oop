26ed066f0ea343f958814bcee1ef030e82b1020f
Ujjal Mahata <ujjadsingh66@gmail.com>
2024-12-06 11:34:32 +0545

    function template

---
+++ functiontemplate.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+void print(int x, int y) { // No need for a template if only handling ints
+    cout << x << " " << y << endl; // Replaced 'ends' with " " for proper spacing
+}
+
+int main() {
+    print(10, 20); // Function call
+    print(a,b);
+    print(2.4,4.5);
+    print(ujjal,mehata)
+    return 0;
+}
+
