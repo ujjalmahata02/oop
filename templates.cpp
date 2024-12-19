9d06545a345d4053fba0a19437bbccbd9209cd57
Ujjal Mahata <ujjadsingh66@gmail.com>
2024-12-19 19:49:33 +0545

    template

--- templates.cpp
+++ templates.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
-using namespace std; 
+using namespace std;
 
+// Function template
 template <typename T>
 T add(T a, T b) {
     return a + b;
 }
 
 int main() {
-    cout << "Sum of integers: " << add(5, 3) << endl;
-    cout << "Sum of doubles: " << add(5.5, 3.3) << endl;
-
+    cout << "Integer addition: " << add(5, 10) << endl;
+    cout << "Double addition: " << add(3.5, 7.2) << endl;
+    cout << "Character addition: " << add('A', 2) << endl; // 'A' (ASCII 65) + 2 = 'C'
     return 0;
 }
